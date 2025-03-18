bf97ecd31d890bb09c523ebd61369560cd06860f
Mike-Philip <xilorgg@gmail.com>
2025-03-18 16:00:08 +0100

    First commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(165, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Memory game";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblInfo.Location = new System.Drawing.Point(115, 510);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(399, 29);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Clik on 3 blok in the same sequence";
+            this.lblInfo.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(259, 541);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(127, 50);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonClickEvent);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExit.Location = new System.Drawing.Point(392, 542);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(115, 50);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonInfo.Location = new System.Drawing.Point(116, 541);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(137, 49);
+            this.buttonInfo.TabIndex = 4;
+            this.buttonInfo.Text = "Statistics";
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            this.buttonInfo.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 603);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonInfo;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int blocksX = 110;
+        int blocksY = 80;
+        int score = 0;
+        int level = 3;
+        int wins = 0;
+        int lose = 0;
+
+        List<PictureBox> pictureBoxes = new List<PictureBox>();
+        List<PictureBox> chosenBoxes = new List<PictureBox>();
+
+        Random rand = new Random();
+
+        Color temp;
+
+        int index = 0;
+        int tries = 0;
+
+        int timeLimit = 0;
+        bool selectingColours = false;
+        
+        string correctOrder = string.Empty;
+        string playerOrder = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetUpBlocks();
+            KeyPreview = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            if (selectingColours)
+            {
+                timeLimit++;
+
+                switch (timeLimit)
+                {
+                    case 10:
+                        temp = chosenBoxes[index].BackColor;
+                        chosenBoxes[index].BackColor = Color.White;
+                        break;
+                    case 20:
+                        chosenBoxes[index].BackColor = temp;
+                        break;
+                    case 30:
+                        chosenBoxes[index].BackColor = Color.White;
+                        break;
+                    case 40:
+                        chosenBoxes[index].BackColor = temp;
+                        break;
+                    case 50:
+                        if (index < chosenBoxes.Count - 1)
+                        {
+                            index++;
+                            timeLimit = 0;
+                        }
+                        else
+                        {
+                            selectingColours = false;
+                        }
+                        break;
+                }
+            }
+            if (tries >= level)
+            {
+                if (correctOrder == playerOrder)
+                {
+                    tries = 0;
+                    GameTimer.Stop();
+                    MessageBox.Show("Well done, you got them correctly.", "Game Says: ");
+                    score++;
+                    wins++;
+                }
+                else
+                {
+                    tries = 0;
+                    GameTimer.Stop();
+                    MessageBox.Show("Your guesses did not match, try again.", "Game Says: ");
+                    lose++;
+                }
+            }
+            lblInfo.Text = "Click on " + level + " blocks in the same sequence.";
+        }
+        private void buttonClickEvent(object sender, EventArgs e)
+        {
+            if (score == 3 && level < 7)
+            {
+                level++;
+                score = 0;
+            }
+
+            correctOrder = string.Empty;
+            playerOrder = string.Empty;
+            chosenBoxes.Clear();
+            chosenBoxes = pictureBoxes.OrderBy(x => rand.Next()).Take(level).ToList();
+
+            for (int i = 0; i < chosenBoxes.Count; i++)
+            {
+                correctOrder += chosenBoxes[i].Name + " ";
+            }
+
+            foreach (PictureBox x in pictureBoxes)
+            {
+                x.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+            }
+
+            Debug.WriteLine(correctOrder);
+            index = 0;
+            timeLimit = 0;
+            selectingColours = true;
+            GameTimer.Start();
+        }
+
+        private void SetUpBlocks()
+        {
+            for (int i = 1; i < 17; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Name = "pic_" + i;
+                newPic.Height = 60;
+                newPic.Width = 60;
+                newPic.BackColor = Color.Black;
+                newPic.Left = blocksX;
+                newPic.Top = blocksY;
+                newPic.Click += ClickOnPictureBox;
+
+                if (i == 4 || i == 8 || i == 12)
+                {
+                    blocksY += 65;
+                    blocksX = 110;
+                }
+                else
+                {
+                    blocksX += 65;
+                }
+                this.Controls.Add(newPic);
+                pictureBoxes.Add(newPic);
+            }
+        }
+
+        private void ClickOnPictureBox(object sender, EventArgs e)
+        {
+            if (!selectingColours && chosenBoxes.Count > 1)
+            {
+                PictureBox temp = sender as PictureBox;
+                temp.BackColor = Color.Black;
+                playerOrder += temp.Name + " ";
+                Debug.WriteLine(playerOrder);
+                tries++;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wins: " + wins +" \nLose: " + lose, "Game Says: ");
+        }
+    }
+}
